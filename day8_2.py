4fb512c8c2c696cb369d85e00051577f1376523d
Rover van der Noort <s.r.vandernoort@student.tudelft.nl>
2023-12-08 13:04:14 +0100

    Add day 8 lcm update

--- day8_2.py
+++ day8_2.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import copy
+from math import lcm
 
 
 with open("input.txt", "r") as f:
@@ -20,17 +22,22 @@ with open("input.txt", "r") as f:
         elif key[2] == "Z":
             ending_nodes.append(key)
 
-    current_nodes = starting_nodes
-    while not all(node in ending_nodes for node in current_nodes):
-        print(current_nodes)
+    current_nodes = copy.copy(starting_nodes)
+    paths = defaultdict(int)
+    while not set(starting_nodes).issubset(paths.keys()):
         for instruction in instructions:
             for i, current in enumerate(current_nodes):
                 if instruction == "L":
                     current = nodes[current][0]
                 elif instruction == "R":
                     current = nodes[current][1]
+                if current in ending_nodes and starting_nodes[i] not in paths.keys():
+                    print(current)
+                    paths[starting_nodes[i]] = steps + 1
                 current_nodes[i] = current
             steps += 1
-            if all(node in ending_nodes for node in current_nodes):
-                break
-    print(steps)
+    print(*paths.values())
+    print(lcm(*paths.values()))
+
+    # too high: 2388568148733210885000
+    # off by one error: add one to reduce by a lot haha
