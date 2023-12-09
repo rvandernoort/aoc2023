f8c43088c3676a140e5d5b49b89748b814a57b82
rvandernoort <s.r.vandernoort@student.tudelft.nl>
2023-12-09 12:02:54 +0100

    add day 9 both parts

---
+++ day9.py
@@ -0,0 +1,27 @@
+
+from collections import defaultdict
+
+
+with open('input.txt') as f:
+    data = f.read().splitlines()
+    histories = []
+    
+    for row in data:
+        seq = [int(num) for num in row.split()]
+        difference = defaultdict(list)
+        depth = 0
+        difference[depth] = seq
+        while not all(item == 0 for item in difference[depth]):
+            depth += 1
+            for s in range(len(difference[depth - 1]) - 1):
+                difference[depth].append(difference[depth - 1][s+1] - difference[depth - 1][s])
+        
+        for i in range(len(difference.items()) - 1, 0, -1):
+                difference[i-1].append(difference[i-1][-1] + difference[i][-1])
+        
+        histories.append(difference[0][-1])
+    
+    print(sum(histories))
+                
+            
+            
